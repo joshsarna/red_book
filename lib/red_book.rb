95075d984b6c622ab067de5269d6d46c1b551686
joshsarna <joshsarna@gmail.com>
2019-03-03 13:17:48 -0800

    fix ActionController bug, add printed lines, add more movie 2 lines, fix red_book method to use @movie_quotes

--- lib/red_book.rb
+++ lib/red_book.rb
@@ -15,48 +15,67 @@ module RedBook
     [
       "I made a promise, Mr. Frodo.",
       "And my axe!",
-      "One does not simply walk into Mordor",
+      "One does not simply walk into Mordor.",
     ],[
-      "They're taking the hobbits to Isengard."
+      "They're taking the hobbits to Isengard.",
+      "Po-tay-toes! Boil 'em, mash 'em, stick 'em in a stew.",
+      "The horn of Helm Hammerhand shall sound in the deep. One last time.",
+      "I want to hear more about Sam. Frodo wouldn't have gotten far without Sam."
     ],[
       "Fool of a Took!"
     ]
   ]
 
   def self.red_book
+    puts "-" * 50
     p @book_quotes.sample.sample
+    puts "-" * 50
   end
 
   def self.red_book1
+    puts "-" * 50
     p @book_quotes[0].sample
+    puts "-" * 50
   end
 
   def self.red_book2
+    puts "-" * 50
     p @book_quotes[1].sample
+    puts "-" * 50
   end
   
   def self.red_book3
+    puts "-" * 50
     p @book_quotes[2].sample
+    puts "-" * 50
   end
 
   def self.red_script
-    p @book_quotes.sample.sample
+    puts "-" * 50
+    p @movie_quotes.sample.sample
+    puts "-" * 50
   end
 
   def self.red_script1
+    puts "-" * 50
     p @movie_quotes[0].sample
+    puts "-" * 50
   end
 
   def self.red_script2
+    puts "-" * 50
     p @movie_quotes[1].sample
+    puts "-" * 50
   end
   
   def self.red_script3
+    puts "-" * 50
     p @movie_quotes[2].sample
+    puts "-" * 50
   end
 end
 
-class ApplicationController
+class ApplicationController < ActionController::Base
   before_action :red
 
   def red
