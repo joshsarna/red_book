6a044cd686a1615f87ab0aa5c09619ca219f31a7
joshsarna <joshsarna@gmail.com>
2019-03-03 08:21:01 -0800

    add at least one quote to each of six sub-arrays

--- lib/red_book.rb
+++ lib/red_book.rb
@@ -2,14 +2,13 @@ module RedBook
   @book_quotes = [
     [
       "When Mr. Bilbo Baggins of Bag End announced that he would shortly be celebrating his eleventy-first birthday with a party of special magnificence, there was much talk and excitement in Hobbiton.",
-      "Deeds will not be less valiant because they are unpraised.",
       "All we have to decide is what to do with the time that is given to us.",
       "All that is gold does not glitter.",
       "Not all those who wander are lost."
     ],[
-
+      "Some of my kin look just like trees now, and need something great to rouse them; and they speak only in whispers. But some of my trees are limb-lithe, and many can talk to me."
     ],[
-
+      "Deeds will not be less valiant because they are unpraised."
     ]
   ]
   @movie_quotes = [
