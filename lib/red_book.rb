834cf206ec8df3fa931d59fcf0140e4a0c5ee98a
joshsarna <joshsarna@gmail.com>
2019-03-02 23:17:03 -0800

    make red_book a module method

--- lib/red_book.rb
+++ lib/red_book.rb
@@ -1,7 +1,7 @@
 module RedBook
   @quotes = ["When Mr. Bilbo Baggins of Bag End announced that he would shortly be celebrating his eleventy-first birthday with a party of special magnificence, there was much talk and excitement in Hobbiton.", "Deeds will not be less valiant because they are unpraised."]
 
-  def red_book
+  def self.red_book
     p @quotes.sample
   end
 end
