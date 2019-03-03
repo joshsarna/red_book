2a041527783aad6aa4ee92b4d7344ad6aecb1eb7
joshsarna <joshsarna@gmail.com>
2019-03-02 23:26:58 -0800

    add quotes, update README.md

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
 ## red_book gem
 
-This gem prints a random Lord of the Rings quote to your rails server every time a route is hit.
+This gem prints a random Lord of the Rings quote to your rails server every time it starts up.
 
 ### Installation
 
-Gemfile: `gem 'red_book', '~> 0.0.2'`
+Gemfile: `gem 'red_book', '~> 0.0.7'`
 
 Install: `gem install red_book`
 
 Add the line `RedBook::red_book` to your routes file:
 
 ```ruby
+RedBook::red_book
 Rails.application.routes.draw do
-  RedBook::red_book
   namespace :api do
     get '/movies' => 'movies#index'
 ```
--- lib/red_book.rb
+++ lib/red_book.rb
@@ -1,5 +1,5 @@
 module RedBook
-  @quotes = ["When Mr. Bilbo Baggins of Bag End announced that he would shortly be celebrating his eleventy-first birthday with a party of special magnificence, there was much talk and excitement in Hobbiton.", "Deeds will not be less valiant because they are unpraised."]
+  @quotes = ["When Mr. Bilbo Baggins of Bag End announced that he would shortly be celebrating his eleventy-first birthday with a party of special magnificence, there was much talk and excitement in Hobbiton.", "Deeds will not be less valiant because they are unpraised.", "All we have to decide is what to do with the time that is given to us.", "All that is gold does not glitter.", "Not all those who wander are lost."]
 
   def self.red_book
     p @quotes.sample
